c4068d965c3ffb1cc8785a40882c29c89ff3d35b
b-vit <46031140+bartak-v@users.noreply.github.com>
2022-04-04 14:45:47 +0200

    2x2 network, sending and receiving new functions

--- oems.cpp
+++ oems.cpp
@@ -55,11 +55,16 @@ void compare_and_save()
     }
 }
 
+void receive_input(int src,int tag1,int tag2,MPI_Request req){
+
+    MPI_Irecv(&x, 1, MPI_INT, src, tag1, MPI_COMM_WORLD, &req);
+    MPI_Irecv(&y, 1, MPI_INT, src, tag2, MPI_COMM_WORLD, &req);
+}
+
 int main(int argc, char *argv[])
 {
     int rank;
     int size;
-    //[x,y] inputs to the node | [L]ower output of the comparator | [H]igher output of the comparator
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -97,20 +102,47 @@ int main(int argc, char *argv[])
     // 4 1x1 sítě
     if (rank < 4)
     {
+        int receiverL,receiverH; //1 send L, 2 send H
         // Input
         MPI_Irecv(&x, 1, MPI_INT, MASTER, TAG_X, MPI_COMM_WORLD, &request);
         MPI_Irecv(&y, 1, MPI_INT, MASTER, TAG_Y, MPI_COMM_WORLD, &request);
+        //receive_input(MASTER,TAG_X,TAG_Y,request);
         compare_and_save();
         printf("I am %d X %d   Y %d  H %d L %d \n", rank, x, y, H, L);
 
         //Send it to the two 2x2 networks | output L H
-        MPI_Isend(&a, 1, MPI_INT, receiver, TAG_X, MPI_COMM_WORLD, &request);
-        MPI_Isend(&b, 1, MPI_INT, receiver, TAG_Y, MPI_COMM_WORLD, &request);
+        if (rank==0 || rank==1) {receiverL = 4; receiverH = 5;}
+        if (rank==2 || rank==3) {receiverL = 6; receiverH = 7;}
+        MPI_Isend(&L, 1, MPI_INT, receiverL, MY_TAG, MPI_COMM_WORLD, &request); // send lower value
+        MPI_Isend(&H, 1, MPI_INT, receiverH, MY_TAG, MPI_COMM_WORLD, &request); // send higher  value
     }
-
+    MPI_Barrier(MPI_COMM_WORLD);
     
 
     // Udělat 2x síť 2x2
+    if(rank < 8 && rank > 3){
+
+        // Input from 1x1 network
+        int sourceX,sourceY,receiverL,receiverH;
+        if (rank==4 || rank==5) {sourceX = 0; sourceY = 1;}
+        if (rank==6 || rank==7) {sourceX = 2; sourceY = 3;}
+        MPI_Irecv(&x, 1, MPI_INT, sourceX, MY_TAG, MPI_COMM_WORLD, &request);
+        MPI_Irecv(&y, 1, MPI_INT, sourceY, MY_TAG, MPI_COMM_WORLD, &request);
+        compare_and_save();
+        printf("I am %d X %d   Y %d  H %d L %d \n", rank, x, y, H, L);
+
+        // Send it to 8,9,10,13
+        if (rank==4) {receiverL = 10; receiverH = 8;}
+        if (rank==5) {receiverL = 8; receiverH = 13;}
+        if (rank==6) {receiverL = 10; receiverH = 9;}
+        if (rank==7) {receiverL = 9; receiverH = 13;}
+        MPI_Isend(&L, 1, MPI_INT, receiverL, MY_TAG, MPI_COMM_WORLD, &request); // send lower value
+        MPI_Isend(&H, 1, MPI_INT, receiverH, MY_TAG, MPI_COMM_WORLD, &request); // send higher  value
+    }
+    // Middle of the 2x2 network
+    if(rank == 8 || rank == 9){
+
+    }
 
     // Udělat 4x4 síť
 
